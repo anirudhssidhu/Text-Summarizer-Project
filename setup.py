f4838084ef7c38ca665b2869ebb93e149c0c5b56
anirudhssidhu <anirudhssidhu@gmail.com>
2023-08-30 16:34:49 +0530

    requirments.txt and setup file

---
+++ setup.py
@@ -0,0 +1,31 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+__version__ = "0.0.0"
+
+REPO_NAME = "Text-Summarizer-Project"
+AUTHOR_USER_NAME = "Anirudh Singh Siddhu"
+SRC_REPO = "textSummarizer"
+AUTHOR_EMAIL = "anirudhssidhu@gmail.com"
+
+setuptools.setup(
+    name=REPO_NAME,
+    version=__version__,
+    author=AUTHOR_USER_NAME,
+    author_email=AUTHOR_EMAIL,
+    description="A simple repository for NLP Project",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
+    project_urls={
+        "Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
+    },
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",]
+)
