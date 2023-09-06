29a649a477126e8d0eeb937533b5906d47955a88
anirudhssidhu <anirudhssidhu@gmail.com>
2023-09-06 17:27:46 +0530

    create data ingestion implementation

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
+from textSummarizer.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from textSummarizer.logging import logger
 
-logger.info("Welcom to our Custom Logger")
+STAGE_NAME = "Data Ingestion Stage"
+try:
+  logger.info(f">>>>>>>> stage {STAGE_NAME} started <<<<<<<< ")
+  data_ingestion = DataIngestionTrainingPipeline()
+  data_ingestion.main()
+  logger.info(f">>>>>>>> stage {STAGE_NAME} completed <<<<<<< \n\nx================x")
+
+except Exception as e:
+  logger.exception(e)
+  raise e
